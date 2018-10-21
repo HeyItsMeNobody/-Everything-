4d05cb237cfc26930663870530e8094633b3b992
HeyItsMeNobody <blomdyon@yahoo.com>
2018-10-21 20:29:22 +0200

    Changed mainRoutes to discordRoutes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const randomHexColor = require('random-hex-color');
-const mainRoutes = require('./mainRoutes.js');
+const discordRoutes = require('./discordRoutes.js');
 
 // View engine
 app.set('view engine', 'ejs');
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 });
 
 // Routes
-app.use('/', mainRoutes);
+app.use('/', discordRoutes);
 
 app.listen(9998, function() {
     console.log('Listening on port 9998 uwu');
